1d24a8319e258baff2335a6f99e0a99a59eef26d
Nguyen Son <ngso.ts@gmail.com>
2022-01-06 08:18:41 +0700

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
 const App = () => {
-  return <div>Rest Countries</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:code" element={<Detail />} />
+    </Routes>
+  );
 };
 
 export default App;
